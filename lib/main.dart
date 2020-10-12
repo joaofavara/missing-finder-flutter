9230af7b62f750c294ea7751db65b99eb1e5eccd
unknown <joaopedro_favara@hotmail.com>
2020-10-12 15:45:20 -0300

    Parte que a raissa fez

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'screen/formulario.dart';
+import 'screen/formularioPessoaDesaparecida.dart';
+import 'screen/formularioPessoaAchada.dart';
+import 'screen/selecaoDoTipo.dart';
+import 'routes/app_routes.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:app/screen/mapScreen.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterConfig.loadEnvVariables();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,37 +38,12 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Button(),
-    );
-  }
-}
-
-class Button extends StatelessWidget {
-  void changeScreen(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return Formulario();
-        }
-      )
-    );
-  }
-
-  @override
-  Widget build( BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-           title: Text('Teste!'),
-        ),
-        body: ButtonBar(
-          children: <Widget>[
-          FlatButton(
-            child: Text('Ok'),
-            color: Colors.blue,
-            onPressed: () => changeScreen(context),
-          ),
-        ]
-      ),
+      routes: {
+        AppRoutes.TIPO_CADASTRO: (ctx) => SelecaoDoTipo() ,
+        AppRoutes.DESAPARECIDA: (ctx) => FormularioComponentPessoaDesaparecida(),
+        AppRoutes.ACHADA: (ctx) => FormularioComponentPessoaAchada(),
+        AppRoutes.MAPA: (ctx) => MapScreen(),
+      },
     );
   }
 }
