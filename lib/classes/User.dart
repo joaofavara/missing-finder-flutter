486fcc875cd25a316c3c1b33e7eddcb84ba8cf8d
unknown <joaopedro_favara@hotmail.com>
2020-10-29 07:26:10 -0300

    feat: criação de classes para ler o json
    - clases de endereço, usuario e uma pessoa desaparecida
    - transforma o json em uma classe com as propriedades necessarias

---
+++ lib/classes/User.dart
@@ -0,0 +1,24 @@
+class User {
+  String email;
+  int id;
+  String nome;
+  int telefone;
+
+  User({this.email, this.id, this.nome, this.telefone});
+
+  User.fromJson(Map<String, dynamic> json) {
+    email = json['email'];
+    id = json['id'];
+    nome = json['nome'];
+    telefone = json['telefone'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['email'] = this.email;
+    data['id'] = this.id;
+    data['nome'] = this.nome;
+    data['telefone'] = this.telefone;
+    return data;
+  }
+}
