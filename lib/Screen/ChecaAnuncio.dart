7c07f8811bdac336d96248881daeab61d4ae0d4b
unknown <joaopedro_favara@hotmail.com>
2020-10-29 08:12:30 -0300

    refactor: checaAnuncio virou anuncio missed person

--- lib/Screen/ChecaAnuncio.dart
+++ lib/Screen/ChecaAnuncio.dart
@@ -1,31 +1,68 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import '../classes/MissedPeople.dart';
 
-// TELA PRA QUANDO CLICA VER UM ANÚNCIO DA HOME
-
+// ignore: must_be_immutable
 class ChecaAnuncio extends StatelessWidget {
+  MissedPeople missedPeople;
+
+  Future<MissedPeople> getPerson(int id) async {
+    print('id: $id');
+    var response = await http.get('http://10.0.2.2:5000/api/people/missed/$id');
+    Map parsed = await jsonDecode(response.body);
+    missedPeople = MissedPeople.fromJson(parsed);
+    return missedPeople;
+  }
+
   @override
   Widget build(BuildContext context) {
-  final anuncio = (ModalRoute.of(context).settings.arguments as Map);
+  final anuncioId = (ModalRoute.of(context).settings.arguments);
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Missing Finder",),
         centerTitle: true,
       ),
-
-      // BODY => GRID COM AS FOTOS
-      body: GridView.count(
-        crossAxisCount: 1, //Grid com 2 colunas
-        children:[
-              Image.network(
-                anuncio['url_imagem'], 
-                width: 500,
-                height: 500,
-                fit: BoxFit.cover
-              ),
-              Text(anuncio.toString()),
-        ]
-      ),
+      body: FutureBuilder<MissedPeople>(
+            future: getPerson(anuncioId), // Fetch the data
+            builder: (_, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                MissedPeople data = snapshot.data;
+                return Column(
+                  children: [
+                    Image.network(
+                      data.urlImagem, 
+                      width: 500,
+                      height: 350,
+                      fit: BoxFit.cover
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          FittedBox(
+                          fit: BoxFit.contain,
+                          child: Text(
+                            'Nome: ${data.nome}',
+                            style: TextStyle(backgroundColor: Colors.grey),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]
+                );
+              } else {
+              // If you have no data, show a progress indicator
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+      )
     );
   }
 }
