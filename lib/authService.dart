455469bcd3311382db1ca98f6fb64eb7c959d082
Raissa Davinha <raissa.furlan@gmail.com>
2020-11-01 10:56:56 -0300

    Add files via upload

--- lib/authService.dart
+++ lib/authService.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:http/http.dart';
 
 loginAuthService(String username, String password) async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'screen/formularioPessoaDesaparecida.dart';
-import 'screen/formularioPessoaAchada.dart';
-import 'screen/selecaoDoTipo.dart';
-import 'routes/app_routes.dart';
-import 'package:flutter_config/flutter_config.dart';
-import 'package:app/screen/mapScreen.dart';
 
+import 'Login.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await FlutterConfig.loadEnvVariables();
-
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,13 +14,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      
-      routes: {
-        AppRoutes.TIPO_CADASTRO: (ctx) => SelecaoDoTipo() ,
-        AppRoutes.DESAPARECIDA: (ctx) => FormularioComponentPessoaDesaparecida(),
-        AppRoutes.ACHADA: (ctx) => FormularioComponentPessoaAchada(),
-        AppRoutes.MAPA: (ctx) => MapScreen(),
-      },
+      home: Login(title: 'Missing finder'),
     );
   }
 }
+
+
